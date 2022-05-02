db7a9b2d727b4a6c02620a03c1a83334dedaa2bf
TravisMoleski <tm769613@ohio.edu>
2022-05-02 13:17:14 -0400

    set_velodyne_params.sh now steps up or down

--- set_velodyne_params_curl.sh
+++ set_velodyne_params_curl.sh
@@ -2,6 +2,36 @@
 # sleep 1
 # curl http://192.168.0.201/cgi/status.json
 # sleep 1
-curl --data "rpm=600" http://192.168.1.201/cgi/setting
+
+init_rpm=600
+rpm_desired=300
+
+if [ $init_rpm -gt $rpm_desired ]
+    then
+        echo "Stepping LiDAR down to ${rpm_desired} from ${init_rpm}..."
+        for ((rpm=$init_rpm; rpm>=$rpm_desired; rpm=rpm-60))
+            do
+                command_rpm="rpm=${rpm}"
+                # curl --data ${command_rpm} http://192.168.1.201/cgi/setting
+                echo
+                echo ${command_rpm}
+                sleep 5
+                curl http://192.168.1.201/cgi/status.json
+            done
+    else
+        echo "Stepping LiDAR up to ${rpm_desired} from ${init_rpm}..."
+        for ((rpm=$init_rpm; rpm>=$rpm_desired; rpm=rpm+60))
+            do
+                command_rpm="rpm=${rpm}"
+                # curl --data ${command_rpm} http://192.168.1.201/cgi/setting
+                echo
+                echo ${command_rpm}
+                sleep 5
+                curl http://192.168.1.201/cgi/status.json
+            done
+fi
 sleep 5
 curl http://192.168.1.201/cgi/status.json
+echo
+echo "Done, RPM at ${command_rpm}..."
+echo
